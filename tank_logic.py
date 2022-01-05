da72cea122502adf4939ecce3fcb04c4aa0eb926
Vlad <v1stone@yandex.ru>
2022-01-05 19:19:50 +0500

    Wrote a tank class base

---
+++ tank_logic.py
@@ -0,0 +1,19 @@
+class Tank:
+    def __init__(self, board, sprite, bullet_sprite, speed,
+                 bullet_speed, bullet_power):
+        self.board = board
+
+    def render(self):
+        pass
+
+    def shot(self):
+        pass
+
+    def move(self, vector):
+        pass
+
+    def rotate(self, angle):
+        pass
+
+    def death(self):
+        pass
