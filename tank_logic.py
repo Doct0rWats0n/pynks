2ea6d91c30c7a830bd3b2081c28ef2ddde10816e
Vlad <v1stone@yandex.ru>
2022-01-08 21:39:39 +0500

    Added the ability to move board

--- tank_logic.py
+++ tank_logic.py
@@ -27,6 +27,7 @@ class GameObject(pygame.sprite.Sprite):
         self.image = sprite
         self.board = board
         self.board.sig_change_size.connect(self.change_sprite_size)
+        self.board.sig_change_view.connect(self.render)
         self.change_sprite_size()
         self.render()
 
